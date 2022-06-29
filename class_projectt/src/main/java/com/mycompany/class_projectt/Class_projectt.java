d50b58ccbdd735d6db97f7ebd4e90d3e6e423146
91706 <91706@10.111.72.128>
2022-06-29 14:16:52 -0400

    ver 1.0

---
+++ class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.class_projectt;
+
+/**
+ *
+ * @author 91706
+ */
+public class Class_projectt {
+
+    public static void main(String[] args) {
+        double[] numArray = { 45.3, 67.5,20.34};
+        double sum = 0.0;
+
+        for (double num: numArray) {
+           sum += num;
+        }
+
+        double average = sum / numArray.length;
+        System.out.format("The average is: %.2f", average);
+    }
+}

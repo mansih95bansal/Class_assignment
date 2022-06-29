ff49cf0ff1f9aa34cd0c662e42844df7566e7579
gurpreet jassi <gurpr@10.111.72.231>
2022-06-29 17:00:20 -0400

    ver 5.1

--- class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
+++ class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
@@ -15,7 +15,7 @@ package com.mycompany.class_projectt;
 public class Class_projectt {
 
     public static void main(String[] args) {
-        double[] numArray = { 45.3, 67.5,20.3, 55};
+        double[] numArray = { 45.3, 42.5, 20.3, 55};
         double sum = 0.0;
 
         for (double num: numArray) {

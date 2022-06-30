f2c9f7cbe526fa9faf70237a2ff2b23f5a97a37c
91706 <91706@10.111.72.128>
2022-06-29 21:22:43 -0400

    ver 1.7

--- class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
+++ class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
@@ -16,12 +16,13 @@ public class Class_projectt {
 
     public static void main(String[] args) {
         double[] numArray = { 45.3, 42.5, 20.3, 55,56,100,151,12,11};
+        double[] numArray = { 45.3, 42.5, 20.3, 55,56,100,151,12,11};
         double sum = 0.0;
 
         for (double num: numArray) {
            sum += num;
         }
-
+                  
         double average = sum / numArray.length;
         
     }

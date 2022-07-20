59722e0ea1ed62d864ae14a40e407e1e47d28a7c
gurkiratsingh5 <gurkiratsingh@localhost>
2022-07-20 17:46:45 -0400

    Add print statment

--- class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
+++ class_projectt/src/main/java/com/mycompany/class_projectt/Class_projectt.java
@@ -22,6 +22,7 @@ public class Class_projectt {
         System.out.println("Hello People");
         System.out.println("Hello People");
         System.out.println("Manish Bansal27 july 2022");
+        System.out.println("Gurkirat Singh 27 july 2022");
 
 
         for (double num: numArray) {
